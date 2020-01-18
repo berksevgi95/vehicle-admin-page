ecf45d0a443294248fa159ca676c0c2cb1df988e
Berk Sevgi <berksevgi95@hotmail.com>
2020-01-18 19:12:58 +0300

    Selectable table implemented

--- src/views/vehicles/VehiclesView.js
+++ src/views/vehicles/VehiclesView.js
@@ -8,12 +8,14 @@ import {
     Icon,
     Menu,
     Sidebar,
+    Checkbox,
 } from 'semantic-ui-react'
 import * as VehiclesActions from './store/vehicles.actions';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux'
 import VehiclesViewForm from './VehiclesViewForm';
 import VehiclesViewFilterForm from './VehiclesViewFilterForm';
+import { List } from 'immutable';
 
 const VehiclesView = ({
     vehicles,
@@ -28,37 +30,82 @@ const VehiclesView = ({
     ...props
 }) => {
 
+    const [selected, setSelected] = React.useState(null);
+
     React.useEffect(() => {
         getVehicles && getVehicles()
 
         return resetVehicles
     }, [])
 
+    const toggleSelection = () => {
+        selected ? setSelected(null) : setSelected(List())
+    }
+
     const handleNavigateDetail = () => {
-        history && history.push('vehicles/detail')
+        !selected && history && history.push('vehicles/detail')
+    }
+
+    const isVehicleChecked = (vehicle) => {
+        return Boolean(selected.filter(item => item.id === vehicle.id).size)
+    }
+
+    const handleClickRow = (vehicle) => {
+        if(selected){
+            !isVehicleChecked(vehicle) ? 
+                setSelected(selected.push(vehicle)) :
+                setSelected(selected.delete(
+                    selected.findIndex(item => item.id === vehicle.id)
+                ))
+        }
+    }
+
+    const handleSelectAllVehicles = () => {
+        selected.size === 0 ?
+            setSelected(vehicles) :
+            setSelected(List()) 
     }
 
     return <Segment basic>
         <div className="flex justify-between">
-            <Header as='h3'>Application Content</Header>
+            <Header as='h3'>Vehicles</Header>
             <div className="flex">
-                <Button compact onClick={openVehicleForm.bind(this, null)}>
-                    <Icon name="add"></Icon>
-                    Add Vehicle
-                </Button>
-                <Button compact onClick={getVehicles} icon="refresh"/>
-                <Button compact onClick={openVehicleFilterbar} icon="filter"/>
+                {selected ? 
+                <React.Fragment>
+                    <Button compact onClick={openVehicleFilterbar} icon="filter" />
+                    <Button compact disabled={selected.size === 0} icon="trash alternate"/>
+                </React.Fragment> : 
+                <React.Fragment>
+                    <Button compact onClick={openVehicleForm.bind(this, null)}>
+                        <Icon name="add"></Icon>
+                        Add Vehicle
+                    </Button>
+                    <Button compact onClick={getVehicles} icon="refresh" />
+                    <Button compact onClick={openVehicleFilterbar} icon="filter" />
+                </React.Fragment>
+                
+                }
+                <Button compact onClick={toggleSelection} icon="tasks" active={Boolean(selected)}/>
             </div>
         </div>
         <Table celled padded selectable>
             <Table.Header>
                 <Table.Row>
+                    {selected && 
+                        <Table.HeaderCell textAlign="center">
+                            <Checkbox 
+                                onChange={handleSelectAllVehicles}
+                                checked={selected.size === vehicles.size}
+                                indeterminate={selected.size > 0 && selected.size !== vehicles.size}
+                            />
+                        </Table.HeaderCell>
+                    }
                     <Table.HeaderCell singleLine>id</Table.HeaderCell>
                     <Table.HeaderCell>Brand</Table.HeaderCell>
                     <Table.HeaderCell>Model Name</Table.HeaderCell>
                     <Table.HeaderCell>req</Table.HeaderCell>
                     <Table.HeaderCell>req_alter</Table.HeaderCell>
-                    <Table.HeaderCell></Table.HeaderCell>
+                    {!selected && <Table.HeaderCell></Table.HeaderCell>}
                 </Table.Row>
             </Table.Header>
 
@@ -66,7 +113,16 @@ const VehiclesView = ({
                 {vehicles &&
                     vehicles.size > 0 &&
                     vehicles.map(vehicle => (
-                        <Table.Row onDoubleClick={handleNavigateDetail} key={vehicle.id}>
+                        <Table.Row 
+                            onClick={handleClickRow.bind(this, vehicle)} 
+                            onDoubleClick={handleNavigateDetail} 
+                            key={vehicle.id}
+                        >
+                            {selected &&
+                                <Table.Cell textAlign="center">
+                                    <Checkbox checked={isVehicleChecked(vehicle)}/>
+                                </Table.Cell>
+                            }
                             <Table.Cell>
                                 {vehicle.id}
                             </Table.Cell>
@@ -82,17 +138,19 @@ const VehiclesView = ({
                             <Table.Cell>
                                 {vehicle.req_alter}
                             </Table.Cell>
-                            <Table.Cell textAlign="center">
-                                <Button 
-                                    size='mini' 
-                                    icon='edit' 
-                                    onClick={openVehicleForm.bind(this, vehicle)}
-                                />
-                                <Button 
-                                    size='mini' 
-                                    icon='delete' 
-                                />
-                            </Table.Cell>
+                            {!selected && 
+                                <Table.Cell textAlign="center">
+                                    <Button 
+                                        size='mini' 
+                                        icon='edit' 
+                                        onClick={openVehicleForm.bind(this, vehicle)}
+                                    />
+                                    <Button 
+                                        size='mini' 
+                                        icon='delete' 
+                                    />
+                                </Table.Cell>
+                            }
                         </Table.Row>
                     ))}
                 
